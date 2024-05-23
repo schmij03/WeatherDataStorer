02bc7f8c6b91ec2cd9f2f95f15fc955f736949dc
schmij03 <96172389+schmij03@users.noreply.github.com>
2024-05-23 16:04:45 +0200

    fixed errors

--- test.py
+++ test.py
@@ -69,6 +69,7 @@ def update_documents():
     updates = []
     for doc in documents:
         region = get_region(doc)
+        print(f"Updating document ID: {doc['_id']} with region: {region}")
         updates.append(UpdateOne({'_id': doc['_id']}, {'$set': {'Region': region}}))
 
     if updates:
